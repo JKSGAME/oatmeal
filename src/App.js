298ab66d8ceb6b8eecf5ce5ac06c7803dfd613c4
Kekoa Wilson <kekoawilson20@gmail.com>
2018-01-16 11:08:09 -0700

    first commit on dash

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Oatmeal</h1>
-        </header>
-        <p className="App-intro">
-        </p>
+        <Dashboard/>
       </div>
     );
   }

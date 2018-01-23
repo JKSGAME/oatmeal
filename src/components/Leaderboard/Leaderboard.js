e17976ec28c652d40c404d5b7add40790bd5bdfe
Kekoa Wilson <kekoawilson20@gmail.com>
2018-01-23 12:28:37 -0700

    tues b4 lunch

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './Leaderboard.css';
 import AgentvAgent from './../AgentvAgent/AgentvAgent';
-import TeamvTeam from './../TeamvTeam/TeamvTeam'
+// import TeamvTeam from './../TeamvTeam/TeamvTeam'
 
 class Leaderboard extends Component {
-    constructor(){
-        super()
+    // constructor(){
+    //     super()
 
-    }
+    // }
     
     render() {
-        {/* Some sort of switch or jsx if statement here to determin which view will get rendered */}
+    {/* Some sort of switch or jsx if statement here to determin which view will get rendered */}
     // return (
     //   <div className="Leaderboard">
     //     <TeamvTeam />
